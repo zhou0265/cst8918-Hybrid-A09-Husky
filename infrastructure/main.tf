fcb228b25059b7eeea424c5ac37bdbf724b45283
zhou0265 <zhou0265@algonquinlive.com>
2025-03-07 20:01:04 -0500

    Fixed AKS cluster formatting and syntax

---
+++ infrastructure/main.tf
@@ -0,0 +1,39 @@
+# main.tf (fixed)
+terraform {
+  required_version = ">= 1.2.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "akscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
